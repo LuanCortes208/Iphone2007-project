98486e59fccd4ec23b75955f115d6db347ecfd6d
Luan Cortes <luancortes208@gmail.com>
2025-04-18 16:56:08 -0300

    feat: criando a exception personalizada

---
+++ src/Exceptions/InterfaceNotImplementedException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class InterfaceNotImplementedException extends Exception{
+
+    public InterfaceNotImplementedException(String message) {
+        super(message);
+    }
+}
